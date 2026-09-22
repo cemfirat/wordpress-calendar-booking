aaf904075e703d5ab89abadae4a76a837b036228
Cem Firat <5923877+cemfirat@users.noreply.github.com>
2026-09-22 16:22:52 +0200

    Generate ICS from canonical UTC booking instants

--- includes/ICS/IcsGenerator.php
+++ includes/ICS/IcsGenerator.php
@@ -6,9 +6,14 @@ use Cemb\Support\Time;
 class IcsGenerator {
     public function generate(array $booking, array $meta, string $title, string $location = '', string $uid = ''): string {
         $uid = $uid !== '' ? $uid : (!empty($booking['booking_uuid']) ? $booking['booking_uuid'] . '@' . wp_parse_url(home_url(), PHP_URL_HOST) : wp_generate_uuid4());
-        $dtstamp = gmdate('Ymd\THis\Z');
-        $dtstart = gmdate('Ymd\THis\Z', strtotime((string)$booking['slot_start']));
-        $dtend = gmdate('Ymd\THis\Z', strtotime((string)$booking['slot_end']));
+        $dtstamp = Time::nowUtc()->format('Ymd\THis\Z');
+        $start = Time::parseUtc((string)($booking['slot_start'] ?? ''));
+        $end = Time::parseUtc((string)($booking['slot_end'] ?? ''));
+        if (!$start || !$end || $end <= $start) {
+            throw new \InvalidArgumentException('Booking contains invalid UTC slot times.');
+        }
+        $dtstart = $start->format('Ymd\THis\Z');
+        $dtend = $end->format('Ymd\THis\Z');
         $description = (string)($meta['message'] ?? ($booking['notes'] ?? ''));
         return "BEGIN:VCALENDAR\r\nVERSION:2.0\r\nPRODID:-//CEMB//Calendar Booking//DE\r\nCALSCALE:GREGORIAN\r\nMETHOD:PUBLISH\r\nBEGIN:VEVENT\r\nUID:" . $this->escape($uid) . "\r\nDTSTAMP:{$dtstamp}\r\nDTSTART:{$dtstart}\r\nDTEND:{$dtend}\r\nSUMMARY:" . $this->escape($title) . "\r\nDESCRIPTION:" . $this->escape($description) . "\r\nLOCATION:" . $this->escape($location) . "\r\nSTATUS:CONFIRMED\r\nEND:VEVENT\r\nEND:VCALENDAR\r\n";
     }
