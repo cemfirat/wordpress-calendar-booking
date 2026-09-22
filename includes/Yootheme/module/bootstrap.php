69026abf13350be95db952cf2d54fa4e2f5a4567
Cem Firat <5923877+cemfirat@users.noreply.github.com>
2026-09-22 15:34:30 +0200

    Register Calendar Booking YOOtheme elements

---
+++ includes/Yootheme/module/bootstrap.php
@@ -0,0 +1,12 @@
+<?php
+
+use YOOtheme\Builder;
+use YOOtheme\Path;
+
+return [
+    'extend' => [
+        Builder::class => function (Builder $builder) {
+            $builder->addTypePath(Path::get('./elements/*/element.php'));
+        },
+    ],
+];
