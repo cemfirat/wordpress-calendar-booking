6fb9a0ff396adb669256e3e7ea78223dd20f4965
Cem Firat <5923877+cemfirat@users.noreply.github.com>
2026-09-22 15:45:41 +0200

    Prepare WordPress Calendar Booking 2.0.0

--- cemb-calendar-booking.php
+++ cemb-calendar-booking.php
@@ -3,7 +3,7 @@
  * Plugin Name: WordPress Calendar Booking
  * Plugin URI: https://github.com/cemfirat/wordpress-calendar-booking
  * Description: Calendar availability and appointment booking with Double Opt-In, optional admin approval, ICS attachments and configurable slots.
- * Version: 1.0.0
+ * Version: 2.0.0
  * Requires at least: 6.5
  * Requires PHP: 8.0
  * Author: Cem Firat
@@ -18,7 +18,7 @@ if (!defined('ABSPATH')) {
     exit;
 }
 
-define('CEMB_VERSION', '1.0.0');
+define('CEMB_VERSION', '2.0.0');
 define('CEMB_FILE', __FILE__);
 define('CEMB_DIR', plugin_dir_path(__FILE__));
 define('CEMB_URL', plugin_dir_url(__FILE__));
@@ -30,7 +30,10 @@ if (is_file($cemb_vendor_autoload)) {
 }
 
 require_once CEMB_DIR . 'includes/Core/Autoloader.php';
+require_once CEMB_DIR . 'includes/Update/GitHubUpdater.php';
 Cemb\Core\Autoloader::register();
+
+new Cemb\Update\GitHubUpdater(CEMB_FILE);
 
 register_activation_hook(__FILE__, ['Cemb\\Core\\Activator', 'activate']);
 register_deactivation_hook(__FILE__, ['Cemb\\Core\\Activator', 'deactivate']);
