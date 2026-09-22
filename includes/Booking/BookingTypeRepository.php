a7a0b1ae457e9219883d8ef912fee1120553a391
Cem Firat <5923877+cemfirat@users.noreply.github.com>
2026-09-22 13:06:20 +0200

    Import booking and calendar baseline

---
+++ includes/Booking/BookingTypeRepository.php
@@ -0,0 +1,23 @@
+<?php
+namespace Cemb\Booking;
+
+class BookingTypeRepository {
+    private string $table;
+    public function __construct() {
+        global $wpdb;
+        $this->table = $wpdb->prefix . 'cemb_booking_types';
+    }
+    public function all(bool $publicOnly = false): array {
+        global $wpdb;
+        $sql = "SELECT * FROM {$this->table} WHERE is_active = 1";
+        if ($publicOnly) {
+            $sql .= ' AND is_public = 1';
+        }
+        $sql .= ' ORDER BY sort_order ASC, name ASC';
+        return $wpdb->get_results($sql);
+    }
+    public function find(int $id) {
+        global $wpdb;
+        return $wpdb->get_row($wpdb->prepare("SELECT * FROM {$this->table} WHERE id = %d", $id));
+    }
+}
