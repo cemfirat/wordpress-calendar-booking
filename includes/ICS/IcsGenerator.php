b535184faa116b81b8e0208b62a09181fd45e0f2
Cem Firat <5923877+cemfirat@users.noreply.github.com>
2026-09-23 15:41:18 +0200

    Include group size in ICS

--- includes/ICS/IcsGenerator.php
+++ includes/ICS/IcsGenerator.php
@@ -10,6 +10,10 @@ class IcsGenerator {
         $dtstart = gmdate('Ymd\THis\Z', strtotime((string)$booking['slot_start']));
         $dtend = gmdate('Ymd\THis\Z', strtotime((string)$booking['slot_end']));
         $description = (string)($meta['message'] ?? ($booking['notes'] ?? ''));
+        $partySize = max(1, (int)($booking['party_size'] ?? 1));
+        if ($partySize > 1) {
+            $description = trim($description . "\nTeilnehmer: " . $partySize);
+        }
         return "BEGIN:VCALENDAR\r\nVERSION:2.0\r\nPRODID:-//WPCB//Calendar Booking//DE\r\nCALSCALE:GREGORIAN\r\nMETHOD:PUBLISH\r\nBEGIN:VEVENT\r\nUID:" . $this->escape($uid) . "\r\nDTSTAMP:{$dtstamp}\r\nDTSTART:{$dtstart}\r\nDTEND:{$dtend}\r\nSUMMARY:" . $this->escape($title) . "\r\nDESCRIPTION:" . $this->escape($description) . "\r\nLOCATION:" . $this->escape($location) . "\r\nSTATUS:CONFIRMED\r\nEND:VEVENT\r\nEND:VCALENDAR\r\n";
     }
 
